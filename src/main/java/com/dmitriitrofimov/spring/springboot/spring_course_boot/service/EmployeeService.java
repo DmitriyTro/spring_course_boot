c391db01f340e3423f25917377562eaae6e6a9b0
Dmitrii Trofimov <probcf@gmail.com>
2021-08-23 18:19:48 +0300

    add entity, controller, dao, service

---
+++ src/main/java/com/dmitriitrofimov/spring/springboot/spring_course_boot/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.dmitriitrofimov.spring.springboot.spring_course_boot.service;
+
+
+import com.dmitriitrofimov.spring.springboot.spring_course_boot.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+	public List<Employee> getAllEmployees();
+
+//	public void saveEmployee(Employee employee);
+//
+//	public Employee getEmployee(int id);
+//
+//	public void deleteEmployee(int id);
+}
