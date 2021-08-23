cbd815b65710a96ebe345a885d297d58ddec6cbd
Dmitrii Trofimov <probcf@gmail.com>
2021-08-23 17:44:18 +0300

    create project structure

---
+++ src/test/java/com/dmitriitrofimov/spring/springboot/spring_course_boot/SpringCourseBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitriitrofimov.spring.springboot.spring_course_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
