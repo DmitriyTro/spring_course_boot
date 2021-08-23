7437b0982c4a5112a529f917abd9547d3a3443e7
Dmitrii Trofimov <probcf@gmail.com>
2021-08-23 20:45:40 +0300

    fix

--- src/main/java/com/dmitriitrofimov/spring/springboot/spring_course_boot/controller/MyRESTController.java
+++ src/main/java/com/dmitriitrofimov/spring/springboot/spring_course_boot/controller/MyRESTController.java
@@ -43,8 +43,6 @@ public class MyRESTController {
 
 	@DeleteMapping("/employees/{id}")
 	public String deleteEmployee(@PathVariable int id) {
-		Employee employee = employeeService.getEmployee(id);
-
 		employeeService.deleteEmployee(id);
 		return "Employee with ID = " + id + " was deleted.";
 	}
